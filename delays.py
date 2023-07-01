41ba7dfd94f38b5c5469fdbdb6660cda1a5f271c
Josh Read <53308348+josh-read@users.noreply.github.com>
2023-07-01 21:38:36 +0100

    Add find_delay method to DelayManager

--- delays.py
+++ delays.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 from pprint import pprint
+from statistics import mean
 
 
 class DelayManager:
@@ -17,6 +18,46 @@ class DelayManager:
         self.delay_adjacency_dict[event][timebase1].update({timebase2: delay})
         self.delay_adjacency_dict[event][timebase2].update({timebase1: -delay})
 
+    def find_delay(self, event, timebase1, timebase2):
+        graph_search_result = graph_search(self.delay_adjacency_dict[event], timebase1, timebase2)
+        try:
+            (_, delay), = graph_search_result
+            return delay
+        except ValueError:
+            print(f'Found {len(graph_search_result)} possible ways of linking {event} '
+                  f'from {timebase1} to {timebase2}:')
+            pprint([path for path, _ in graph_search_result])
+            delays = [delay for _, delay in graph_search_result]
+            print(f'Taking the average delay from {delays}.')
+            return mean(delays)
+
+
+def graph_search(adjacency_list, start_node, finish_node):
+    """Variation on the depth first search algorithm which sums up the edge values as
+    it traverses the graph, and doesn't stop until all reachable nodes have been
+    visited, returning all possible paths."""
+
+    visited = set()
+    all_paths = []
+
+    def dfs(graph, node, path, path_sum):  # function for dfs
+
+        path.append(node)
+
+        if node == finish_node:
+            all_paths.append((list(path), path_sum))
+
+        if node not in visited:
+            visited.add(node)
+            for neighbour, delay in graph[node].items():
+                dfs(graph, neighbour, path, path_sum+delay)
+
+        path.pop()
+
+    dfs(adjacency_list, start_node, path=[], path_sum=0)
+
+    return all_paths
+
 
 def main():
     dm = DelayManager()
@@ -26,6 +67,7 @@ def main():
     dm.add('e1', 'tb1', 'tb4', 70)
     dm.add('e2', 'tb1', 'tb4', 140)
     pprint(dm.delay_adjacency_dict)
+    print(dm.find_delay('e1', 'tb1', 'tb4'))
 
 
 if __name__ == '__main__':
