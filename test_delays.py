7b454ad4f45740bcd178a266bdc39a3260289d00
Josh Read <53308348+josh-read@users.noreply.github.com>
2023-07-02 14:37:58 +0100

    Add tests for find_delay with under and over constrained cases

--- test_delays.py
+++ test_delays.py
@@ -1,4 +1,14 @@
 from delays import DelayManager
+import pytest
+
+
+def test_no_route():
+    dm = DelayManager.from_list([
+        ('e1', 'tb1', 'tb2', 100),
+        ('e1', 'tb4', 'tb3', 250),
+    ])
+    with pytest.raises(ValueError):
+        dm.find_delay('e1', 'tb1', 'tb4')
 
 
 def test_single_route():
@@ -6,6 +16,17 @@ def test_single_route():
         ('e1', 'tb1', 'tb2', 100),
         ('e1', 'tb2', 'tb3', 25),
         ('e1', 'tb4', 'tb3', 250),
+    ])
+    delay = dm.find_delay('e1', 'tb1', 'tb4')
+    assert delay == -125
+
+
+def test_two_routes():
+    dm = DelayManager.from_list([
+        ('e1', 'tb1', 'tb2', 100),
+        ('e1', 'tb2', 'tb3', 25),
+        ('e1', 'tb4', 'tb3', 250),
+        ('e1', 'tb1', 'tb4', -125),
     ])
     delay = dm.find_delay('e1', 'tb1', 'tb4')
     assert delay == -125
